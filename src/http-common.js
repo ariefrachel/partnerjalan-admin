5c44003d728ff52ba58cacbc499e06b9620bdecb
Arief Rachman <ariefrachel33@gmail.com>
2022-12-01 09:11:49 +0700

    Updated

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://d08f-103-191-109-11.ap.ngrok.io/",
+  baseURL: "https://88f8-103-191-109-11.ap.ngrok.io/",
   headers: {
     "Content-type": "application/json"
   }
