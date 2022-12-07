66c562e2ca41af4005473d950f1912e97c8c6fc2
Arief Rachman <ariefrachel33@gmail.com>
2022-12-07 12:46:14 +0700

    Updated

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://plain-views-cry-103-191-109-11.loca.lt/",
+  baseURL: "https://eleven-pens-thank-103-191-109-11.loca.lt/",
 
   headers: {
     "Content-type": "application/json"
