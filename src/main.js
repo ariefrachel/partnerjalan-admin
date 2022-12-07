053c30bfbc29bfdab75235a9451940f6021b3282
Arief Rachman <ariefrachel33@gmail.com>
2022-12-07 13:06:21 +0700

    Updated

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import './assets/css/style.css'
 
 Vue.config.productionTip = false
 
-Vue.prototype.$pathApi = "https://eleven-pens-thank-103-191-109-11.loca.lt/"
+Vue.prototype.$pathApi = "https://wild-clocks-grow-103-191-109-11.loca.lt/"
 
 
 import AOS from 'aos';
