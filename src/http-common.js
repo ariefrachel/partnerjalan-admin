4a0c1f6a3641a29544704a9b2e6cb17100b975e3
Arief Rachman <ariefrachel33@gmail.com>
2022-12-07 13:19:45 +0700

    Updated

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://wild-clocks-grow-103-191-109-11.loca.lt/",
+  baseURL: "https://jalan.loca.lt/",
 
   headers: {
     "Content-type": "application/json"
